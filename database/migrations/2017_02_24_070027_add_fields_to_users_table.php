ea299ca5b6aacd9a0dfe8ad403608b70d08f966e
vothithuy <thuyvo0903@gmail.com>
2017-02-24 16:41:01 +0700

    -f

---
+++ database/migrations/2017_02_24_070027_add_fields_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->after('password')->nullable();
+            $table->string('fullname')->after('avatar')->nullable();
+            $table->string('address')->after('fullname')->nullable();
+            $table->string('phone')->after('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('fullname');
+            $table->dropColumn('address');
+            $table->dropColumn('phone');
+        });
+    }
+}
---
+++ database/migrations/2017_02_24_083334_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id');
+            $table->double('price');
+            $table->integer('amount');
+            $table->string('image');
+            $table->boolean('inventory');
+            $table->double('avg_rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
